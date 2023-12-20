f1186a7b878e3d43ec9738bd00b2052ce48af6b9
LirijeShabani <LirijeShabani@LAPTOP-GRKV5HSB>
2023-12-20 15:25:04 +0100

    menu in the making

--- components/Sidemenu.vue
+++ components/Sidemenu.vue
@@ -36,6 +36,13 @@ function handleSearch() {
 function toggleMenu() {
   showWholeMenu.value = !showWholeMenu.value;
 }
+
+const randomReviews = computed(() => {
+    const reviewsToUse = searchResults.value.length > 0 ? searchResults.value : items;
+  const shuffledReviews = reviewsToUse.slice().sort(() => Math.random() - 0.5);
+  const selectedReviews = shuffledReviews.slice(0, 2);
+  return selectedReviews;
+});
 </script>
 
 <template>
@@ -43,8 +50,8 @@ function toggleMenu() {
         <div class="sidemenu__container" :class="{ 'sidemenu__container--closed': !showWholeMenu }">
             <div v-if="showWholeMenu" class="sidemenu__container-open-menu">
                 <div class="sidemenu__container-open-menu-header">
-                    <button class="sidemenu-btn" @click="toggleMenu"><font-awesome-icon :icon="['fas', 'angle-left']" /></button>
-                    <h2 class="sidemenu__container-open-menu-header-title">sidemenu</h2>
+                    <button class="sidemenu-btn" @click="toggleMenu"><font-awesome-icon :icon="['fas', 'angles-left']" /></button>
+                    <h2 class="sidemenu__container-open-menu-header-title"></h2>
                 </div>
                 <div class="sidemenu__container-open-menu-search">
                     <input 
@@ -56,15 +63,48 @@ function toggleMenu() {
                         placeholder="search.." 
                     /> 
                 </div>
+                <div class="sidemenu__container-open-menu-review">
+                    <div class="sidemenu__container-open-menu-review-header">
+                        <h5>New reviews</h5>
+                        <NuxtLink to="/" class="sidemenu__container-open-menu-review-header-showall">show all ></Nuxtlink>
+                    </div>
+                    <div v-for="review in randomReviews" :key="review.id" class="sidemenu__container-open-menu-categories-larg-preview">
+                        <div class="preview-card">
+                            <h5>{{ review.name }}</h5>
+                            <p>{{ review.comment }}</p>
+                            <!-- Add other review details as needed -->
+                        </div>
+                    </div>
+                </div>
+                <div class="sidemenu__container-open-menu-review">
+                    <div class="sidemenu__container-open-menu-review-header">
+                        <h5>Published reviews</h5>
+                        <NuxtLink to="/" class="sidemenu__container-open-menu-review-header-showall">show all ></Nuxtlink>
+                    </div>
+                </div>
+                <div  class="sidemenu__container-open-menu-review">
+                    <div class="sidemenu__container-open-menu-review-header">
+                        <h5>Unpublished reviews</h5>
+                        <NuxtLink to="/" class="sidemenu__container-open-menu-review-header-showall">show all ></Nuxtlink>
+                    </div>
+                </div>
             </div>
+            <!-- closed menu -->
             <div v-if="!showWholeMenu" class="sidemenu__container-closed-menu">
-                <button class="sidemenu-btn" @click="toggleMenu"><font-awesome-icon :icon="['fas', 'angle-right']" /></button>
+                <button class="sidemenu-btn" @click="toggleMenu"><font-awesome-icon :icon="['fas', 'angles-right']" /></button>
                 <div class="sidemenu__container-closed-menu-icons">
                     <font-awesome-icon 
-                class="sidemenu__container-closed-menu-search-icon" 
-                :icon="['fas', 'magnifying-glass']"  
-                @click="handleSearch" 
-            />
+                        class="sidemenu__container-closed-menu-search-icon" 
+                        :icon="['fas', 'magnifying-glass']"  
+                        @click="handleSearch" 
+                    />
+                    <font-awesome-icon 
+                        class="font-awesome-icon" 
+                        :icon="['fas', 'xmark']" 
+                    />
+                    <font-awesome-icon 
+                        :icon="['fas', 'check']" 
+                    />
                 </div>
             </div>
             <!-- need to move this eventually to a new page -->
@@ -80,7 +120,7 @@ function toggleMenu() {
 .sidemenu {
     position: relative;
     min-height: 100vh;
-    width: 250px;
+    width: 300px;
 
     &__container {
         display: flex;
@@ -109,12 +149,6 @@ function toggleMenu() {
                 align-items: center;
                 justify-content: start;
 
-                // .sidemenu-btn {
-                //     position: absolute;
-                //     top: 0;
-                //     left: 0;
-                // }
-
                 &-title {
                     font-size: 24px;
                 }
@@ -125,6 +159,70 @@ function toggleMenu() {
                 align-items: center;
                 justify-content: center;
                 margin: 10px 0;
+            }
+
+            &-review {
+                display: flex;
+                flex-direction: column;
+                margin: 15px 0;
+
+                &-header {
+                    display: flex;
+                    justify-content: space-between;
+                    width: 100%;
+                    margin-bottom: 2px;
+
+                    &-showall {
+                        font-size: 12px;
+                    }
+                }
+            }
+
+            &-categories {
+                display: flex;
+                flex-direction: column;
+                justify-content: space-between;
+                margin: 15px 0;
+                
+                &-larg-preview {
+                    display: flex;
+                    align-items: center;
+                    justify-content: center;
+                    width: 100%;
+                    height: 100px;
+                    border: 1px solid $color-cloud-gray;
+                    border-radius: 10px;
+                    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+                    margin-bottom: 20px;
+
+                    &:last-child {
+                        margin-bottom: 0;
+                    }
+                }
+
+                &-link {
+                    display: flex;
+                    align-items: center;
+                    justify-content: space-between;
+                    width: 100%;
+                    height: 40px;
+                    border: 1px solid $color-cloud-gray;
+                    border-radius: 10px;
+                    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+                    margin-bottom: 20px;
+
+                    &:last-child {
+                        margin-bottom: 0;
+                    }
+
+                    &-content {
+                        display: flex;
+                        align-items: center;
+                        justify-content: space-around;
+                        width: 100%;
+                        height: 100%;
+                    }
+                }
             }
         }
 
